2f6a4fb5dbfab833c628beee4e451b6246214ca5
Brian Pompey <brianthescholar@gmail.com>
2020-03-17 01:29:56 +0000

    added student views, controllers, and model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,36 +2,85 @@ class StudentsController < ApplicationController
 
   # GET: /students
   get "/students" do
-    erb :"/students/index.html"
+    if logged_in?
+      @students = Student.
+      erb :'students/my_students'
+    else
+      redirect to '/login'
+    end
   end
 
   # GET: /students/new
   get "/students/new" do
-    erb :"/students/new.html"
+    if logged_in?
+      erb :'students/new'
+    else
+      redirect to '/login'
+    end
   end
 
   # POST: /students
   post "/students" do
-    redirect "/students"
-  end
-
-  # GET: /students/5
-  get "/students/:id" do
-    erb :"/students/show.html"
+    if logged_in?
+      if params[:student_name] == "" || params[:student_birthday] == ""
+        redirect to '/students/new'
+      else
+        @student = @current_user.students.create(:student_name => params[:student_name], :student_birthday => params[:student_birthday])
+        @student.save
+        redirect to '/my_students'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   # GET: /students/5/edit
   get "/students/:id/edit" do
-    erb :"/students/edit.html"
+    if logged_in?
+      @student = Student.find_by_id(params[:id])
+      if @student && @student.teacher == current_user
+        erb :'students/edit_students'
+      else
+        erb :'/my_students'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   # PATCH: /students/5
   patch "/students/:id" do
-    redirect "/students/:id"
+    if logged_in?
+      if params[:student_name] == "" || params[:student_birthday] == ""
+        redirect to '/students/#{params[:id]}/edit'
+      else
+        @student = Student.find_by_id(params[:id])
+        if @student && @student.teacher == current_user
+          if @student.update(student_name: params[:student_name]) || (student_birthday: params[:student_birthday])
+            redirect to "/students/#{@student.id}"
+          else
+            redirect to "/students/#{@student.id}/edit"
+          end
+        else
+          redirect to '/my_students'
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   # DELETE: /students/5/delete
   delete "/students/:id/delete" do
-    redirect "/students"
+    if logged_in?
+      @student = Student.find_by_id(params[:id])
+      if @student && @student.teacher == current_user
+        @student.delete
+      end
+      redirect to '/my_students'
+    else
+      redirect to '/login'
+    end
   end
+  
 end
