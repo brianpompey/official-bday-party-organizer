4bedfbe775d21b1d2e6f1bc0dd58ff0b7cd96e2b
Brian Pompey <brianthescholar@gmail.com>
2020-03-18 16:51:25 +0000

    working on parties controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,27 +2,47 @@ class PartiesController < ApplicationController
 
   # GET: /parties
   get "/parties" do
-    erb :"/parties/index.html"
+    @parties = Party.all
+    erb :"/parties/parties"
   end
 
   # GET: /parties/new
   get "/parties/new" do
-    erb :"/parties/new.html"
+    if logged_in?
+      erb :'parties/new'
+    else
+      redirect to '/login'
+    end
   end
 
   # POST: /parties
   post "/parties" do
-    redirect "/parties"
-  end
-
-  # GET: /parties/5
-  get "/parties/:id" do
-    erb :"/parties/show.html"
+    if logged_in?
+      if params[:venue] == ""
+        redirect to '/students/new'
+      else
+#        @student = @teacher.students.new(:student_name => params[:student_name], :student_birthday => params[:student_birthday])
+        @party = Party.new(:venue => params[:venue], :student_id => current_user.student.id, :teacher_id => current_user.id)
+        @party.save
+        redirect to '/parties'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   # GET: /parties/5/edit
   get "/parties/:id/edit" do
-    erb :"/parties/edit.html"
+    if logged_in?
+      @party = Party.find_by_id(params[:id])
+      if @party && @party.student == current_user.student
+        erb :'parties/edit_parties'
+      else
+        erb :'/parties'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   # PATCH: /parties/5
@@ -32,6 +52,10 @@ class PartiesController < ApplicationController
 
   # DELETE: /parties/5/delete
   delete "/parties/:id/delete" do
-    redirect "/parties"
+    @party = Party.find_by_id(params[:id])
+    if @party && @party.student == current_user.student
+      @party.delete
+    end
+    redirect to '/parties'
   end
 end
