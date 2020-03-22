a55121a25a2ef46f5cc12c13c648153f432785ab
Brian Pompey <brianthescholar@gmail.com>
2020-03-22 17:17:54 +0000

    put the flash messages in the teacher controller

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -1,31 +1,31 @@
 class TeachersController < ApplicationController
 
   get '/signup' do
-#    if !logged_in?
+    if !logged_in?
       erb :'teachers/signup'
-#    else
-#      redirect to '/my_students'
-#    end
+    else
+      redirect to '/my_students'
+    end
   end
 
   post '/signup' do
-#    if params[:name] == "" || params[:email] == "" || params[:password] == ""
-#      redirect to '/signup', locals: {message: "Please fill out properly"}
-      #error message should show up on the page
-#    else
-    @teacher = Teacher.new(name: params["name"], email: params["email"], password: params["password"])
-    @teacher.save
-    session[:user_id] = @teacher.id
-    redirect to '/students'
-#    end
+    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+      flash[:error] = "Invalid input, please try again"
+      redirect to '/signup'
+    else
+      @teacher = Teacher.new(name: params["name"], email: params["email"], password: params["password"])
+      @teacher.save
+      session[:user_id] = @teacher.id
+      redirect to '/students'
+    end
   end
 
   get '/login' do
-#    if !logged_in?
+    if !logged_in?
       erb :'teachers/login'
-#    else
-#      redirect to '/my_students'
-#    end
+    else
+      redirect to '/my_students'
+    end
   end
 
   post '/login' do
@@ -34,17 +34,17 @@ class TeachersController < ApplicationController
       session[:user_id] = @teacher.id
       redirect to '/students'
     else
+      flash[:error] = "Invalid information. Please Sign up"
       redirect to '/signup'
     end
   end
 
   get '/logout' do
-#    if logged_in?
+    if logged_in?
       erb :'teachers/logout'
-
-#    else
-#      redirect to '/'
-#    end
+    else
+      redirect to '/'
+    end
   end
 
   post '/logout' do
