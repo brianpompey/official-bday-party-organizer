d3bc8a001d609e33a1a61fe86315652f81271a99
brianpompey <brianthescholar@gmail.com>
2020-03-25 20:19:13 -0400

    added features

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
-teachers_list = [
-  [ "Matt Hardy", "matthardy@flat.edu", "hardip" ],
-  [ "Chelsea Matthews", "chelmatt@flat.edu", "mattchel" ],
-  [ "Robert Fredericks", "rfreddy@flat.edu", "robfred" ],
-  [ "James Wilkos", "jwilkos@flat.edu", "jwilly" ],
-  [ "Barry White", "bwhite@flat.edu", "barry93" ]
-]
-teachers_list.each do |name, email, password|
-  Teacher.create( name: name, email: email, password: password )
-end
+#teachers_list = [
+#  [ "Matt Hardy", "matthardy@flat.edu", "hardip" ],
+ # [ "Chelsea Matthews", "chelmatt@flat.edu", "mattchel" ],
+#  [ "Robert Fredericks", "rfreddy@flat.edu", "robfred" ],
+#  [ "James Wilkos", "jwilkos@flat.edu", "jwilly" ],
+#  [ "Barry White", "bwhite@flat.edu", "barry93" ]
+#]
+#teachers_list.each do |name, email, password|
+#  Teacher.create( name: name, email: email, password: password )
+#end
+
+Teacher.create(:name => "Matt Hardy", :email => "matthardy@flat.edu", :password_digest => "hardip")
+
+Teacher.create(:name => "Chelsea Matthews", :email => "chelmatt@flat.edu", :password_digest => "mattchel")
+
+Teacher.create(:name => "Robert Fredericks", :email => "rfreddy@flat.edu", :password_digest => "robfred")
+
+Teacher.create(:name => "James Wilkos", :email => "jwilkos@flat.edu", :password_digest => "jwilly")
+
+Teacher.create(:name => "Barry White", :email => "bwhite@flat.edu", :password_digest => "barry93")
+
+puts "Success: Theme data loaded"
