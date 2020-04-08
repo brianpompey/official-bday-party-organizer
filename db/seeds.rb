56b07a54de711ee096cb4036314e0b54fbb91fb6
brianpompey <brianthescholar@gmail.com>
2020-04-07 21:01:32 -0400

    lab done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,4 @@
-#teachers_list = [
-#  [ "Matt Hardy", "matthardy@flat.edu", "hardip" ],
- # [ "Chelsea Matthews", "chelmatt@flat.edu", "mattchel" ],
-#  [ "Robert Fredericks", "rfreddy@flat.edu", "robfred" ],
-#  [ "James Wilkos", "jwilkos@flat.edu", "jwilly" ],
-#  [ "Barry White", "bwhite@flat.edu", "barry93" ]
-#]
-#teachers_list.each do |name, email, password|
-#  Teacher.create( name: name, email: email, password: password )
-#end
+
 
 Teacher.create(:name => "Matt Hardy", :email => "matthardy@flat.edu", :password => "hardip")
 
@@ -19,22 +10,22 @@ Teacher.create(:name => "James Wilkos", :email => "jwilkos@flat.edu", :password 
 
 Teacher.create(:name => "Barry White", :email => "bwhite@flat.edu", :password => "barry93")
 
-Student.create(:student_name => "Jimmy Green", :student_birthday => 2010-10-5, :teacher_id => 13)
+Student.create(:student_name => "Jimmy Green", :student_birthday => "2010-10-5", :teacher_id => 13)
 
-Student.create(:student_name => "Lonnie Parker", :student_birthday => 2010-6-8, :teacher_id => 13)
+Student.create(:student_name => "Lonnie Parker", :student_birthday => "2010-6-8", :teacher_id => 13)
 
-Student.create(:student_name => "Tim Hall", :student_birthday => 2010-3-5, :teacher_id => 14)
+Student.create(:student_name => "Tim Hall", :student_birthday => "2010-3-5", :teacher_id => 14)
 
-Student.create(:student_name => "Pam Hollis", :student_birthday => 2010-4-4, :teacher_id => 14)
+Student.create(:student_name => "Pam Hollis", :student_birthday => "2010-4-4", :teacher_id => 14)
 
-Student.create(:student_name => "Brad Wilson", :student_birthday => 2010-7-8, :teacher_id => 15)
+Student.create(:student_name => "Brad Wilson", :student_birthday => "2010-7-8", :teacher_id => 15)
 
-Student.create(:student_name => "Terry Chan", :student_birthday => 2010-11-11, :teacher_id => 15)
+Student.create(:student_name => "Terry Chan", :student_birthday => "2010-11-11", :teacher_id => 15)
 
-Student.create(:student_name => "Mike Campbell", :student_birthday => 2010-5-5, :teacher_id => 16)
+Student.create(:student_name => "Mike Campbell", :student_birthday => "2010-5-5", :teacher_id => 16)
 
-Student.create(:student_name => "Larry Thomas", :student_birthday => 2010-6-6, :teacher_id => 16)
+Student.create(:student_name => "Larry Thomas", :student_birthday => "2010-6-6", :teacher_id => 16)
 
-Student.create(:student_name => "Cal Webber", :student_birthday => 2010-7-7, :teacher_id => 17)
+Student.create(:student_name => "Cal Webber", :student_birthday => "2010-7-7", :teacher_id => 17)
 
-Student.create(:student_name => "Jeff Williams", :student_birthday => 2010-4-3, :teacher_id => 17)
+Student.create(:student_name => "Jeff Williams", :student_birthday => "2010-04-06", :teacher_id => 17)
