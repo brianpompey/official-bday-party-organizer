f7058d1fbec8045b3ffec4168b86d0a70e4621bc
brianpompey <brianthescholar@gmail.com>
2020-04-07 20:53:42 -0400

    final touches

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,23 @@ Teacher.create(:name => "Robert Fredericks", :email => "rfreddy@flat.edu", :pass
 Teacher.create(:name => "James Wilkos", :email => "jwilkos@flat.edu", :password => "jwilly")
 
 Teacher.create(:name => "Barry White", :email => "bwhite@flat.edu", :password => "barry93")
+
+Student.create(:student_name => "Jimmy Green", :student_birthday => 2010-10-5, :teacher_id => 13)
+
+Student.create(:student_name => "Lonnie Parker", :student_birthday => 2010-6-8, :teacher_id => 13)
+
+Student.create(:student_name => "Tim Hall", :student_birthday => 2010-3-5, :teacher_id => 14)
+
+Student.create(:student_name => "Pam Hollis", :student_birthday => 2010-4-4, :teacher_id => 14)
+
+Student.create(:student_name => "Brad Wilson", :student_birthday => 2010-7-8, :teacher_id => 15)
+
+Student.create(:student_name => "Terry Chan", :student_birthday => 2010-11-11, :teacher_id => 15)
+
+Student.create(:student_name => "Mike Campbell", :student_birthday => 2010-5-5, :teacher_id => 16)
+
+Student.create(:student_name => "Larry Thomas", :student_birthday => 2010-6-6, :teacher_id => 16)
+
+Student.create(:student_name => "Cal Webber", :student_birthday => 2010-7-7, :teacher_id => 17)
+
+Student.create(:student_name => "Jeff Williams", :student_birthday => 2010-4-3, :teacher_id => 17)
